80f9dd829bfda1ada4e799c7b1e3a760f04a10b5
umer ashraf <umer ashraf>
2017-03-11 16:31:01 +0100

    GUI API integration

--- src/com/clustbox/clustering/Cluster.java
+++ src/com/clustbox/clustering/Cluster.java
@@ -5,11 +5,11 @@ import java.io.FileWriter;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Properties;
 import java.io.IOException;
 import java.util.Random;
-
 
 import java.io.InputStream;
 import java.io.FileInputStream;
@@ -40,143 +40,115 @@ import net.sf.javaml.tools.weka.FromWekaUtils;
 import net.sf.javaml.clustering.evaluation.AICScore;
 //import net.sf.javaml.utils.ArrayUtils;
 
-class bestCentroidResult {
-	Instance[] bestCent
-}
-
 public class Cluster {
-	static Dataset data = null;
-	static DistanceMeasure dm;	
-	static HashMap<String,String> map;
-	static CIndex CIdx;
-	static ClusterEvaluationWithNaturalFitness dbEval = new DaviesBouldinScore();
-	
-//	public enum Algo{
-//		SimpleKMeans, IterativeKMeans, SimpleKMedoids, IterativeKMedoids
-//	}
+	public Dataset data = null;
+	public DistanceMeasure dm;
+	public HashMap<String, String> map;
+	public List evalMetrics;
+	public CIndex CIdx;
+	public ClusterEvaluationWithNaturalFitness dbEval = new DaviesBouldinScore();
 
-	public static void main(String[] args) throws IOException {
-		
+	// public enum Algo{
+	// SimpleKMeans, IterativeKMeans, SimpleKMedoids, IterativeKMedoids
+	// }
+
+	public void runClustering(HashMap formElements) throws IOException {
+
 		Clusterer km;
-		/* Load the config file */
-		map = getProperty();
-		
-		/* Load a dataset */
-		data = FileHandler.loadDataset(new File(map.get("dataFile")), 4, ",");
+		evalMetrics = getEvalMetrics(formElements);
+		dm = getSimilarityMeasure(formElements.get("similarityMeasure"));
 
-		//data = ARFFHandler.loadARFF( new File(map.get("dataFile")) );
-		
-		/* Set the distance measure */
-		if(map.get("similarityMeasure").equals("CosineSimilarity"))
-			dm = new CosineSimilarity();
-		else if(map.get("similarityMeasure").equals("JaccardIndexSimilarity"))
-			dm = new JaccardIndexSimilarity();
-		else if(map.get("similarityMeasure").equals("ManhattanDistance"))
-			dm = new ManhattanDistance();	
-		else if(map.get("similarityMeasure").equals("MinkowskiDistance"))
-			dm = new MinkowskiDistance();
-		else if(map.get("similarityMeasure").equals("NormalizedEuclideanDistance"))
-			dm = new NormalizedEuclideanDistance(data);						
-		else if(map.get("similarityMeasure").equals("PearsonCorrelationCoefficient"))
-			dm = new PearsonCorrelationCoefficient();				
-		else 
-			dm = new EuclideanDistance();
-		
+		/* Load a dataset */
+		data = FileHandler.loadDataset(new File((String) formElements.get("dataFile")), 4, ",");
+
 		CIdx = new CIndex(dm);
-		
+
 		CreateOuputDir("Output");
 		File sFile = new File("Output/sFile.csv");
 		FileWriter fWrite = new FileWriter(sFile);
 		/* Run Simple KMeans or KMedoids with input K or centroids and return */
-		if(map.containsKey("SimpleKMeans") || map.containsKey("SimpleKMedoids"))
-		{
+		if (formElements.containsKey("simpleKMeans") || formElements.containsKey("simpleKMedoids")) {
 			int k;
 			Instance[] centroids = null;
-			if(map.containsKey("noOfClusters")){
-				k = Integer.parseInt(map.get("noOfClusters"));
-			}
-			else if(map.containsKey("centroidsSource")){
-				Dataset initCentroidsDS = FileHandler.loadDataset(new File(map.get("centroidsSource")), 4, ",");
+			if (formElements.containsKey("noOfClusters")) {
+				k = Integer.parseInt((String) formElements.get("noOfClusters"));
+			} else if (formElements.containsKey("centroidsSource")) {
+				Dataset initCentroidsDS = FileHandler
+						.loadDataset(new File((String) formElements.get("centroidsSource")), 4, ",");
 				ArrayList<Instance> initCentroids = new ArrayList<Instance>();
-				for(Instance ins: initCentroidsDS)
-				{
+				for (Instance ins : initCentroidsDS) {
 					initCentroids.add(ins);
 				}
 				centroids = initCentroids.toArray(new Instance[0]);
 				k = initCentroids.size();
-			}
-			else{
+			} else {
 				System.out.println("Error: Please provide either K or initial centroids for Simle KMeans/KMedoids run");
 				return;
 			}
-			
+
 			/* Run the KMeans or KMedoids Algo */
-			if(map.containsKey("SimpleKMedoids")){
+			if (formElements.containsKey("SimpleKMedoids")) {
 				km = new CBKMedoids(k, 100, dm, centroids);
-			}
-			else {
+			} else {
 				km = new CBKMeans(k, 100, dm, centroids);
 			}
-			
+
 			Dataset[] clusters = km.cluster(data);
-			int cnt=0;
-			for(Dataset clust: clusters)
-			{
+			int cnt = 0;
+			for (Dataset clust : clusters) {
 				cnt++;
-				FileHandler.exportDataset(clust,new File("Output/Cluster-" + cnt + ".data"),false, ",");
+				FileHandler.exportDataset(clust, new File("Output/Cluster-" + cnt + ".data"), false, ",");
 				System.out.println("Dumped cluster data to Output/Cluster-" + cnt + ".data");
 			}
-			return;			
+			return;
 		}
-		
-		/* Run the Iterative KMeans/KMedoids algorithms*/
-		else if (map.containsKey("IterativeKMeans") || map.containsKey("IterativeKMedoids")){
+
+		/* Run the Iterative KMeans/KMedoids algorithms */
+		else if (formElements.containsKey("iterativeKMeans") || formElements.containsKey("iterativeKMedoids")) {
 			int k;
 			/* If K is given only run best Centroids assessment */
-			if(map.containsKey("noOfClusters")){
-				k = Integer.parseInt(map.get("noOfClusters"));
+			if (formElements.containsKey("noOfClusters")) {
+				k = Integer.parseInt((String) formElements.get("noOfClusters"));
 				Instance[] bestCentroids = bestCentroids(k);
 				return;
-			}
-			else {
+			} else {
 				int KMIN = 2;
-				int KMAX = data.size()/2;
+				int KMAX = data.size() / 2;
 				ClusterEvaluation tempEval = new AICScore();
 				IterativeKMeans ikm = new IterativeKMeans(2, 5, tempEval);
 				Dataset[] iClusters = ikm.cluster(data);
 				int cnt = 0;
-				for(Dataset clust: iClusters)
-				{
+				for (Dataset clust : iClusters) {
 					cnt++;
-					FileHandler.exportDataset(clust,new File("Output/ClusterT-" + cnt + ".data"),false, ",");
+					FileHandler.exportDataset(clust, new File("Output/ClusterT-" + cnt + ".data"), false, ",");
 				}
 				System.out.println("IKMeans k = " + iClusters.length);
-				
-				double bestScore = -1; //Initialize of least possible score
+
+				double bestScore = -1; // Initialize of least possible score
 				Dataset[] bestClusters = null;
-				for(k=KMIN; k<KMAX; k++){
+				for (k = KMIN; k < KMAX; k++) {
 					Instance[] bestCentroids = bestCentroids(k);
-					if(map.containsKey("IterativeKMedoids")){
+					if (formElements.containsKey("iterativeKMedoids")) {
 						km = new CBKMedoids(k, 100, dm, bestCentroids);
-					}
-					else {
+					} else {
 						km = new CBKMeans(k, 100, dm, bestCentroids);
 					}
 					Dataset[] tempClusters = km.cluster(data);
-					//double tempScore = CIdx.score(tempClusters);
+					// double tempScore = CIdx.score(tempClusters);
 					double temp = intraCluster(tempClusters, dm);
-					double tempScore = getAvgSilhouetteValues(tempClusters, dm);//tempEval.score(tempClusters);//getAvgSilhouetteValues(tempClusters, dm);
+					double tempScore = getAvgSilhouetteValues(tempClusters, dm);// tempEval.score(tempClusters);//getAvgSilhouetteValues(tempClusters,
+																				// dm);
 					/* Save the first run output as best data */
-					if(k == KMIN){
+					if (k == KMIN) {
 						bestScore = tempScore;
 						bestClusters = tempClusters;
 					}
 					System.out.println("Score with k=" + k + " is " + tempScore);
 					fWrite.append(String.valueOf(tempScore));
-					if(k<KMAX-1)
+					if (k < KMAX - 1)
 						fWrite.append("\n");
-					//if( tempEval.compareScore(bestScore, tempScore)) {
-					if(tempScore > bestScore){
+					// if( tempEval.compareScore(bestScore, tempScore)) {
+					if (tempScore > bestScore) {
 						bestScore = tempScore;
 						bestClusters = tempClusters;
 					}
@@ -184,178 +156,140 @@ public class Cluster {
 				fWrite.flush();
 				fWrite.close();
 				System.out.println("Best Score is:" + bestScore);
-				cnt=0;
-				for(Dataset clust: bestClusters)
-				{
+				cnt = 0;
+				for (Dataset clust : bestClusters) {
 					cnt++;
-					FileHandler.exportDataset(clust,new File("Output/Cluster-" + cnt + ".data"),false, ",");
+					FileHandler.exportDataset(clust, new File("Output/Cluster-" + cnt + ".data"), false, ",");
 					System.out.println("Dumped cluster data to Output/Cluster-" + cnt + ".data");
 				}
 			}
 		}
-	
 	}
-	
-	protected static void CreateOuputDir(String dirStr){
+
+	private List getEvalMetrics(HashMap formElements) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	private DistanceMeasure getSimilarityMeasure(Object similarityElement) {
+		DistanceMeasure dm;
+		if (similarityElement.equals("CosineSimilarity"))
+			dm = new CosineSimilarity();
+		else if (similarityElement.equals("JaccardIndexSimilarity"))
+			dm = new JaccardIndexSimilarity();
+		else if (similarityElement.equals("ManhattanDistance"))
+			dm = new ManhattanDistance();
+		else if (similarityElement.equals("MinkowskiDistance"))
+			dm = new MinkowskiDistance();
+		else if (similarityElement.equals("NormalizedEuclideanDistance"))
+			dm = new NormalizedEuclideanDistance(data);
+		else if (similarityElement.equals("PearsonCorrelationCoefficient"))
+			dm = new PearsonCorrelationCoefficient();
+		else
+			dm = new EuclideanDistance();
+		return dm;
+	}
+
+	protected void CreateOuputDir(String dirStr) {
 		File dir = new File(dirStr);
-		if(dir.exists()){
-			String[]entries = dir.list();
-			for(String s: entries){
-				File currentFile = new File(dir.getPath(),s);
+		if (dir.exists()) {
+			String[] entries = dir.list();
+			for (String s : entries) {
+				File currentFile = new File(dir.getPath(), s);
 				currentFile.delete();
 			}
-		}
-		else {
+		} else {
 			dir.mkdir();
 		}
 	}
-	
-	protected static Instance[] bestCentroids(int k){
+
+	protected Instance[] bestCentroids(int k) {
 		Instance[] bestCentroids = null;
 		double bestScore = 0;
 		Dataset[] bestClusters;
-		for(int i=0; i<2; i++){
+		for (int i = 0; i < 2; i++) {
 			Random rg = new Random(System.currentTimeMillis());
 			Dataset Centroids = DatasetTools.bootstrap(data, k, rg);
 			Clusterer km;
 			ArrayList<Instance> initCentroids = new ArrayList<Instance>();
-			for(Instance ins: Centroids)
-			{
+			for (Instance ins : Centroids) {
 				initCentroids.add(ins);
 			}
 			Instance[] tempCentroids = initCentroids.toArray(new Instance[0]);
 			km = new CBKMeans(k, 100, dm, bestCentroids);
 			Dataset[] tempClusters = km.cluster(data);
 			double tempScore = getAvgSilhouetteValues(tempClusters, dm);
-			//System.out.println("bestC: " + tempScore);
-			if(tempScore > bestScore) {
+			// System.out.println("bestC: " + tempScore);
+			if (tempScore > bestScore) {
 				bestScore = tempScore;
 				bestCentroids = tempCentroids;
 			}
 		}
 		return bestCentroids;
-		
+
 	}
 
-	protected static double getAvgSilhouetteValues(Dataset[] datas, DistanceMeasure dm) {
+	protected double getAvgSilhouetteValues(Dataset[] datas, DistanceMeasure dm) {
 		ArrayList<Double> sillhouettes = new ArrayList<Double>();
 		Double s = 0.0;
 		for (int i = 0; i < datas.length; i++) {
-			for(int j = 0; j < datas[i].size(); j++) {
+			for (int j = 0; j < datas[i].size(); j++) {
 				Instance x = datas[i].instance(j);
 				double Dw = 0, avgDw = 0, De = 0, avgDe = 0, nearDe = Double.MAX_VALUE;
-				for(int k=0; k < datas[i].size();k++) {
-//					if(k == j)
-//						continue;
+				for (int k = 0; k < datas[i].size(); k++) {
+					// if(k == j)
+					// continue;
 					Instance y = datas[i].instance(k);
-					if(!y.equals(x))
-						Dw += dm.measure(x, datas[i].instance(k)); 
+					if (!y.equals(x))
+						Dw += dm.measure(x, datas[i].instance(k));
 				}
 				avgDw = Dw / (datas[i].size() - 1);
-				
-												
-				for(int l = 0; l < datas.length; l++){ 
-					if( l == i)
+
+				for (int l = 0; l < datas.length; l++) {
+					if (l == i)
 						continue;
 					De = 0;
-					for(int m=0; m < datas[l].size(); m++){
+					for (int m = 0; m < datas[l].size(); m++) {
 						Instance y = datas[l].instance(m);
-//						if(datas[l].contains(x))
-//							continue;
+						// if(datas[l].contains(x))
+						// continue;
 						De += dm.measure(x, datas[l].instance(m));
 					}
 					avgDe = De / datas[l].size();
-					if(avgDe <  nearDe)
+					if (avgDe < nearDe)
 						nearDe = avgDe;
 				}
-				
-				if(nearDe < avgDw)
-					s = ( nearDe - avgDw ) / avgDw;
+
+				if (nearDe < avgDw)
+					s = (nearDe - avgDw) / avgDw;
 				else
-					s = ( nearDe - avgDw ) / nearDe;
-				
+					s = (nearDe - avgDw) / nearDe;
+
 				if (Double.isNaN(s))
 					s = 0.0;
-				
+
 				sillhouettes.add(s);
-				
+
 			}
 		}
-		
+
 		return calculateAverage(sillhouettes);
 
-		
-		
 	}
-	
-	protected static double calculateAverage(ArrayList<Double> Sills) {
+
+	protected double calculateAverage(ArrayList<Double> Sills) {
 		Double sum = 0.0;
 		Double avg = 0.0;
-		if(!Sills.isEmpty()) {
+		if (!Sills.isEmpty()) {
 			for (Double s : Sills) {
 				sum += s;
 			}
-			avg =  sum / Sills.size();
+			avg = sum / Sills.size();
 		}
 		return avg.doubleValue();
 	}
-	
-//	protected static double getAvgSilhouetteValue(Dataset[] datas, DistanceMeasure dm) {
-//	
-//		double silhouette = 0;
-//		for (int i = 0; i < datas.length; i++) {
-//			double a = 0;
-//			double b = 0;
-//			for(int j = 0; j < datas[i].size(); j++) {
-//				
-//				double dw = 0, fw = 0;
-//				Instance x = datas[i].instance(j);
-//				for (int k = 0; k < datas[i].size(); k++) {
-//					if(k == j)
-//						continue;
-//					Instance y = datas[i].instance(k);
-//					double distance = dm.measure(x, y);
-//					dw += distance;
-//					fw++;
-//				}
-//				a += dw/fw;
-//				
-//				dw = 0;
-//				fw = 0;
-//				
-//				for(int k = 0; k < datas.length; k++){
-//					for(int l = 0; l < datas[k].size(); l++){
-//						Instance y = datas[k].instance(l);
-//						if(!datas[i].contains(y)){
-//							double distance = dm.measure(x, y);
-//							dw += distance;
-//							fw++;
-//							
-//						}
-//					}
-//				}
-//				
-//				double newb = dw/fw;
-//				if(j == 0) b = newb;
-//				if(newb<b)
-//					b = newb;
-//			}
-//			if(a<b)
-//				silhouette = (b-a) / b;
-//			else
-//				silhouette = (b-a) / a;
-//			
-//			
-//			silhouette = silhouette/2;
-//		}
-//		
-//		return silhouette;
-//		
-//	}
-//	
-	
-	
-	protected static double intraCluster(Dataset[] datas, DistanceMeasure dm) {
+
+	protected double intraCluster(Dataset[] datas, DistanceMeasure dm) {
 		double dw = 0, fw = 0;
 
 		for (int i = 0; i < datas.length; i++) {
@@ -375,7 +309,7 @@ public class Cluster {
 		return wb;
 	}
 
-	protected static double interCluster(Dataset[] datas, DistanceMeasure dm) {
+	protected double interCluster(Dataset[] datas, DistanceMeasure dm) {
 		double db = 0, fb = 0;
 
 		for (int i = 0; i < datas.length; i++) {
@@ -397,25 +331,22 @@ public class Cluster {
 		double wb = (db / fb);
 		return wb;
 	}
-	
-	protected static HashMap<String,String> getProperty()
-    {
-        Properties prop = new Properties();
-        HashMap<String,String>map = new HashMap<String,String>();
-        try
-        {
-            FileInputStream inputStream = new FileInputStream(new File("data/test.prop"));
-            prop.load(inputStream);
-        }
-        catch (Exception e) {
-            e.printStackTrace();
-            System.out.println("Some issue finding or loading file....!!! " + e.getMessage());
 
-        }
-        for(String key : prop.stringPropertyNames()) {
-        	map.put(key, prop.getProperty(key)); 
-        }
-        return map;
-    }
-	
+	protected HashMap<String, String> getProperty() {
+		Properties prop = new Properties();
+		HashMap<String, String> map = new HashMap<String, String>();
+		try {
+			FileInputStream inputStream = new FileInputStream(new File("data/test.prop"));
+			prop.load(inputStream);
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.out.println("Some issue finding or loading file....!!! " + e.getMessage());
+
+		}
+		for (String key : prop.stringPropertyNames()) {
+			map.put(key, prop.getProperty(key));
+		}
+		return map;
+	}
+
 }
